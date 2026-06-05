1274ffe389903b010d9e7ea54dd95a7ef27c100b
Olivier <oattia@gmail.com>
2026-06-05 10:17:44 +0000

    Add Node.js server

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.dirname(__filename)));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.listen(PORT, () => console.log(`Running on port ${PORT}`));
